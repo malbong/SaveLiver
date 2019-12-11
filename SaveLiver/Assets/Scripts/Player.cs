b92cc4ca488805d50d2ad9a5c136fe6b379dc7aa
Inyong <tlsdlsdyd98@naver.com>
2019-12-11 21:13:27 +0900

    Player 싱글톤

---
+++ SaveLiver/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public static Player instance { get; set; }
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this; // instance 초기화
+        }
+        DontDestroyOnLoad(gameObject); // 씬이 새롭게 로드되어도 변경(파괴)되지 않도록.
+    }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
