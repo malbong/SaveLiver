01cea0b6cea837f5a15d772ff9df2ee764e1a962
gon3578 <gon3578@gmail.com>
2020-01-03 22:00:05 +0900

    1. Button 예외 처리 (GameManager 수정)

--- SaveLiver/Assets/Scripts/GameManager.cs
+++ SaveLiver/Assets/Scripts/GameManager.cs
@@ -29,6 +29,12 @@ public class GameManager : MonoBehaviour
 
     private Color originColor; //원래 카메라 색 임시저장
 
+    private bool pauseButtonFadeOutRunning = false;
+    private bool pauseButtonFadeInRunning = false;
+    private bool joyStickFadeOutRunning = false;
+    private bool pausePanelFadeInRunning = false;
+    private bool pausePanelFadeOutRunning = false;
+
 
     private void Awake()
     {
@@ -104,11 +110,9 @@ public class GameManager : MonoBehaviour
 
     public void OnPause() //pause button에 onClick에 달아줌
     {
-        if (isPause == true) //이미 pause라면 리턴
-        {
-            Debug.Log("Already Paused");
-            return;
-        }
+        if (pausePanelFadeOutRunning || pauseButtonFadeInRunning) return;
+
+        if (isPause) return;
 
         originTimeScale = Time.timeScale; //저장해놓기
         Time.timeScale = 0f;
@@ -120,28 +124,26 @@ public class GameManager : MonoBehaviour
         Camera.main.backgroundColor = new Color(0.5f, 0.5f, 0.5f); //컬러를 회색으로 바꿈
 
         pauseButton.GetComponent<AudioSource>().Play();
-        //다른 탭 동작 처리
-        StartCoroutine(PausePanelFadeIn());
 
-        StartCoroutine(JoyStickFadeOut()); //JoyStick 닫기
+        //다른 탭 동작 처리
         StartCoroutine(PauseButtonFadeOut());//PauseButton 닫기
+        StartCoroutine(JoyStickFadeOut()); //JoyStick 닫기
+        StartCoroutine(PausePanelFadeIn());
     }
 
 
     public void OnContinue() //pause panel에 play버튼에 onClick 달아줌
     {
-        if (isPause == false)
-        {
-            Debug.Log("Already playing");
-            return;
-        }
+        if (pauseButtonFadeOutRunning || joyStickFadeOutRunning || pausePanelFadeInRunning) return;
+
+        if (!isPause) return;
 
         Time.timeScale = originTimeScale; //원래의 타임스케일로 돌려줌
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
 
         isPause = false;
 
-        StartCoroutine(PausePanelFadeOut(null)); //퍼즈 패널 꺼줌
+        StartCoroutine(PausePanelFadeOut()); //퍼즈 패널 꺼줌
         StartCoroutine(PauseButtonFadeIn()); //퍼즈 버튼 다시 켜줌
         joyStickTouchArea.SetActive(true); //조이스틱 다시 켜줌
 
@@ -151,11 +153,7 @@ public class GameManager : MonoBehaviour
 
     public void OnReload() //pause panel에 reload버튼에 onClick 달아줌
     {
-        if (!isPause)
-        {
-            Debug.Log("진행중이라 리로드 불가능");
-            return;
-        }
+        if (!isPause) return;
 
         Time.timeScale = 1.0f; //처음의 타임스케일로 돌려줌
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
@@ -163,17 +161,15 @@ public class GameManager : MonoBehaviour
 
         Camera.main.backgroundColor = originColor; //카메라색 원래대로 돌려줌
 
-        StartCoroutine(PausePanelFadeOut("ReLoad")); //퍼즈패널 꺼주기
+        StartCoroutine(PausePanelFadeOut()); //퍼즈패널 꺼주기
+
+        StartCoroutine(LoadSceneAfterWaiting("Play Scene"));
     }
 
 
     public void OnHomeButtonClick()
     {
-        if (!isPause)
-        {
-            Debug.Log("진행중이라 홈 이동 불가능");
-            return;
-        }
+        if (!isPause) return;
 
         Time.timeScale = 1.0f; //처음의 타임스케일로 돌려줌
         Time.fixedDeltaTime = 0.02f * Time.timeScale;
@@ -181,7 +177,9 @@ public class GameManager : MonoBehaviour
 
         Camera.main.backgroundColor = originColor; //카메라색 원래대로 돌려줌
 
-        StartCoroutine(PausePanelFadeOut("Home"));
+        StartCoroutine(PausePanelFadeOut());
+
+        StartCoroutine(LoadSceneAfterWaiting("Menu Scene"));
     }
 
 
@@ -193,9 +191,15 @@ public class GameManager : MonoBehaviour
 
     private IEnumerator PauseButtonFadeOut()
     {
+        if (pausePanelFadeOutRunning || pauseButtonFadeInRunning) {
+            yield break;
+        }
+
+        pauseButtonFadeOutRunning = true;
+
         Image image = pauseButton.GetComponent<Image>();
         Color tmpColor = image.color;
-        while (isPause)
+        while (true)
         {
             pauseButton.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f); //Scale과 color를 변경함
             tmpColor.a -= 0.1f;
@@ -206,27 +210,31 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSecondsRealtime(0.01f); //RealTime으로 쉬면 Time.timeScale = 0 이어도 코루틴 진행가능
         }
 
-        if (isPause)
-        {
-            //button 끄기 SetActive(false)
-            pauseButton.SetActive(false);
-            pauseButton.GetComponent<Button>().enabled = false;
-        }
+        //button 끄기 SetActive(false)
+        pauseButton.SetActive(false);
+        pauseButton.GetComponent<Button>().enabled = false;
+        
+        pauseButtonFadeOutRunning = false;
     }
 
 
     private IEnumerator PauseButtonFadeIn()
     {
+        if (pauseButtonFadeOutRunning || joyStickFadeOutRunning || pausePanelFadeInRunning)
+        {
+            yield break;
+        }
+
+        pauseButtonFadeInRunning = true;
+
         Image image = pauseButton.GetComponent<Image>();
         Color tmpColor = image.color;
 
-        if (!isPause)
-        {
-            //button 켜기 SetActive(true)
-            pauseButton.SetActive(true);
-        }
+        //button 켜기 SetActive(true)
+        pauseButton.SetActive(true);
+        
 
-        while (!isPause)
+        while (true)
         {
             pauseButton.transform.localScale += new Vector3(0.1f, 0.1f, 0.1f); //Scale과 color를 변경함
             tmpColor.a += 0.1f;
@@ -242,12 +250,21 @@ public class GameManager : MonoBehaviour
         image.color = tmpColor;
         pauseButton.transform.localScale = new Vector3(1f, 1f, 1f);
         pauseButton.GetComponent<Button>().enabled = true;
+
+        pauseButtonFadeInRunning = false;
     }
 
 
     private IEnumerator JoyStickFadeOut()
     {
-        while (isPause)
+        if (pausePanelFadeOutRunning || pauseButtonFadeInRunning)
+        {
+            yield break;
+        }
+
+        joyStickFadeOutRunning = true;
+
+        while (true)
         {
             joyStickTouchArea.transform.localScale -= new Vector3(0.1f, 0.1f, 0.1f); //Scale을 줄임
 
@@ -255,24 +272,27 @@ public class GameManager : MonoBehaviour
 
             yield return new WaitForSecondsRealtime(0.01f);
         }
-
-        //멈춘 상태라면 잠시 꺼주고 상태를 돌려놓음
-        if (isPause)
-        {
-            joyStickTouchArea.SetActive(false);
-        }
+        
+        joyStickTouchArea.SetActive(false);
+        
         joyStickTouchArea.transform.localScale = new Vector3(1f, 1f, 1f);
+
+        joyStickFadeOutRunning = false;
     }
 
 
     private IEnumerator PausePanelFadeIn()
     {
-        if (isPause)
+        if (pausePanelFadeOutRunning || pauseButtonFadeInRunning)
         {
-            pausePanel.SetActive(true);
+            yield break;
         }
 
-        while (isPause)
+        pausePanelFadeInRunning = true;
+
+        pausePanel.SetActive(true);
+        
+        while (true)
         {
             //pausePanel Scale(0 -> 1)
             pausePanel.transform.localScale += new Vector3(0.2f, 0.2f, 0.2f);
@@ -281,12 +301,21 @@ public class GameManager : MonoBehaviour
 
             yield return new WaitForSecondsRealtime(0.01f);
         }
+
+        pausePanelFadeInRunning = false;
     }
 
 
-    private IEnumerator PausePanelFadeOut(string where)
+    private IEnumerator PausePanelFadeOut()
     {
-        while (!isPause) //진행중이라면 isPause == false
+        if (pauseButtonFadeOutRunning || joyStickFadeOutRunning || pausePanelFadeInRunning)
+        {
+            yield break;
+        }
+
+        pausePanelFadeOutRunning = true;
+
+        while (true) //진행중이라면 isPause == false
         {
             //pausePanel Scale(1 -> 0)
             pausePanel.transform.localScale -= new Vector3(0.2f, 0.2f, 0.2f);
@@ -295,12 +324,32 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSecondsRealtime(0.01f);
         }
 
-        if (!isPause)
+        pausePanel.SetActive(false);
+
+        pausePanelFadeOutRunning = false;
+    }
+
+
+    private IEnumerator LoadSceneAfterWaiting(string sceneName)
+    {
+        while (sceneName == "Play Scene")
         {
-            pausePanel.SetActive(false);
+            if (!pausePanelFadeOutRunning)
+            {
+                SceneManager.LoadScene(sceneName);
+                yield break;
+            }
+            yield return new WaitForSecondsRealtime(0.01f);
         }
 
-        if (where == "ReLoad") SceneManager.LoadScene("Play Scene");
-        if (where == "Home") SceneManager.LoadScene("Menu Scene");
+        while (sceneName == "Menu Scene")
+        {
+            if (!pausePanelFadeOutRunning)
+            {
+                SceneManager.LoadScene(sceneName);
+                yield break;
+            }
+            yield return new WaitForSecondsRealtime(0.01f);
+        }
     }
 }
